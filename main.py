ef96413f27c175fa5032037a023fa831a3f80934
tjirmaine <tjirmaine@gmail.com>
2022-05-10 22:05:19 +0200

    Can output ordered notes
    annotations are ordered by recency for some reason, so that needs to be fixed as notes appear in the wrong order

--- main.py
+++ main.py
@@ -10,42 +10,31 @@ from webcolors import (
 def main():
     doc = fitz.open("sample.pdf")
     print(len(doc))
-
-    page = doc[0]
-
-    highlights = []
-
-    annot = page.firstAnnot
-    while annot:
-        if annot.type[0] == 8:
-            colours = annot.colors
-            print(colours)
-            if len(colours.get('stroke')) == 3:
-                rgb = [round(x * 250) for x in colours.get('stroke')]
-                # named_colour = rgb_to_name(rgb, spec='css3')
-                named_colour = convert_rgb_to_names(rgb)
-                print(named_colour)
-            all_coordinates = annot.vertices
-            if len(all_coordinates) == 4:
-                highlight_coord = fitz.Quad(all_coordinates).rect
-                highlights.append(highlight_coord)
-            else:
-                all_coordinates = [all_coordinates[x:x + 4] for x in
-                                   range(0, len(all_coordinates), 4)]
-                for i in range(0, len(all_coordinates)):
-                    coord = fitz.Quad(all_coordinates[i]).rect
-                    highlights.append(coord)
-        annot = annot.next
-
-    all_words = page.get_text_words()
-
     # List to store all the highlighted texts
     highlight_text = []
-    for h in highlights:
-        sentence = [w[4] for w in all_words if fitz.Rect(w[0:4]).intersect(h)]
-        highlight_text.append(" ".join(sentence))
+    for pageNumber in range(0, len(doc)):
+        page = doc[pageNumber]
+        highlights = []
 
-    print(highlight_text)
+        annot = page.firstAnnot
+        while annot:
+            if annot.type[0] == 8:
+                colours = annot.colors
+                print(colours)
+                if len(colours.get('stroke')) == 3:
+                    rgb = [round(x * 250) for x in colours.get('stroke')]
+                    # named_colour = rgb_to_name(rgb, spec='css3')
+                    named_colour = convert_rgb_to_names(rgb)
+                    print(named_colour)
+                highlights.append((annot.rect, named_colour))
+            annot = annot.next
+        #
+        all_words = page.get_text_words()
+
+        for h in highlights:
+            sentence = [w[4] for w in all_words if fitz.Rect(w[0:4]).intersects(h[0])]
+            highlight_text.append((" ".join(sentence), h[1]))
+    print_text(highlight_text)
 
 
 def convert_rgb_to_names(rgb_tuple):
@@ -60,6 +49,18 @@ def convert_rgb_to_names(rgb_tuple):
     kdt_db = KDTree(rgb_values)
     distance, index = kdt_db.query(rgb_tuple)
     return names[index]
+
+
+def print_text(highlighted_text):
+    with open('output.txt', 'w') as output:
+        for sentence in highlighted_text:
+            colour = sentence[1]
+            if colour == 'khaki':
+                output.write("-" + sentence[0] + "\n")
+            elif colour == 'tomato':
+                output.write("   -" + sentence[0] + "\n")
+            elif colour == 'plum':
+                output.write("       -" + sentence[0] + "\n")
 
 
 if __name__ == "__main__":
